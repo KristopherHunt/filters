abe5743020d2e8847c1852fc9da7133fae54b4b1
eghunt <eghunt@ttu.ee>
2024-04-19 01:28:49 +0300

    Refactor api

--- backend/src/main/java/com/ekhunt/backend/controller/CriteriaController.java
+++ backend/src/main/java/com/ekhunt/backend/controller/CriteriaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/filters")
+@RequestMapping("/api/v1/criteria")
 public class CriteriaController {
 
     private final CriteriaService criteriaService;
